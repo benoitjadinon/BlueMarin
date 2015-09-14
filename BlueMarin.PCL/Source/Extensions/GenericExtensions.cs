adca7f603e0dc7200ec749f81def924a279fb40a
Benoit Jadinon <raz-l@raz-l.com>
2015-09-14 11:26:20 +0200

    added a generic Chain() !

---
+++ BlueMarin.PCL/Source/Extensions/GenericExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BlueMarin
+{
+	public static class GenericExtensions
+	{
+		// http://arstechnica.com/civis/viewtopic.php?t=127188
+		/* usage :
+		Properties.Settings.Default
+			.Chain(s => s.User = currentUser)
+			.Save();
+		 */
+		public static T Chain<T>(this T @this, Action<T> action) 
+			where T : class
+		{
+			if (action != null)
+				action(@this);
+			return @this;
+		}
+	}
+}
+
