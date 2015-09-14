8d7c7949464a120d0b8038904d96811eb6dfa3c0
Benoit Jadinon <raz-l@raz-l.com>
2015-09-15 01:21:03 +0200

    added reverse

--- BlueMarin.PCL/Source/Extensions/StringExtensions.cs
+++ BlueMarin.PCL/Source/Extensions/StringExtensions.cs
@@ -4,19 +4,22 @@ namespace BlueMarin
 {
 	public static class StringExtensions
 	{
-		public static bool IsNullOrBlank(this String text)
+		public static bool IsNullOrBlank(this String @this)
 		{
-			return text == null || text.Trim().Length == 0 || text == "";
+			return @this == null || @this.Trim().Length == 0 || @this == "";
 		}
 
-		public static bool IsEmpty(this String text)
+		public static bool IsEmpty(this String @this)
 		{
-			return text.IsNullOrBlank ();
+			return @this.IsNullOrBlank ();
 		}
 
-		public static string ToMD5(this String text)
+		public static string ToMD5(this String @this)
 		{
-			return MD5Core.GetHashString (text);
+			if (@this == null)
+				return null;
+
+			return MD5Core.GetHashString (@this);
 		}
 
 		public static string OrEmpty(this String text)
@@ -26,8 +29,21 @@ namespace BlueMarin
 
 		public static string Substring (this string @this, string startString, string endString)
 		{
+			if (@this == null)
+				return null;
+
 			var start = @this.IndexOf (startString) + startString.Length;
 			return @this.Substring (start, @this.IndexOf (endString, start) - start);
+		}
+
+		public static string Reverse (this string @this)
+		{
+			if (@this == null)
+				return null;
+			
+			char[] charArray = @this.ToCharArray();
+			Array.Reverse( charArray );
+			return new string( charArray );
 		}
 	}
 }
