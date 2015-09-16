0ae899b9a60e56bcebe496b8d3e953ffd96fbef8
Benoit Jadinon <raz-l@raz-l.com>
2015-09-16 02:40:15 +0200

    added DisplayMetricsExtensions

---
+++ BlueMarin.Android/Source/Extensions/DisplayMetricsExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BlueMarin.Android
+{
+	public static class DisplayMetricsExtensions
+	{
+		public static int ConvertPixelsToDp(this global::Android.Util.DisplayMetrics @this, float pixelValue)
+		{
+			return (int) (pixelValue/@this.Density);
+		}
+	}
+}
+
--- BlueMarin.Android/Source/ObjectTypeHelper.cs
+++ BlueMarin.Android/Source/ObjectTypeHelper.cs
@@ -7,7 +7,7 @@ namespace BlueMarin.Android
 		public static T Cast<T>(this Java.Lang.Object obj) where T : class
 		{
 			var propertyInfo = obj.GetType().GetProperty("Instance");
-			return propertyInfo == null ? null : propertyInfo.GetValue(obj, null) as T;
+			return propertyInfo?.GetValue(obj, null) as T;
 		}
 	}
 }
