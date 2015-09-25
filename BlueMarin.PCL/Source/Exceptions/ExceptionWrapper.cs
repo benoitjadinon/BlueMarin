45ea7905d3c2de5c16bb4a1f9275052b26cce48a
Benoit Jadinon <raz-l@raz-l.com>
2015-09-25 10:17:06 +0200

    IgnoreException + DisposableBoolean

---
+++ BlueMarin.PCL/Source/Exceptions/ExceptionWrapper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace BlueMarin
+{
+	public static class ExceptionWrapper
+	{
+		public static void IgnoreException (Action action, bool print = true)
+		{
+			try {
+				action ();
+			} catch (Exception e) {
+				//ignore
+				Debug.WriteLine (e);
+			}
+		}
+
+		public static T IgnoreException<T> (Action<T> action, bool print = true)
+		{
+			try {
+				action ();
+			} catch (Exception e) {
+				//ignore
+				Debug.WriteLine (e);
+			}
+		}
+
+		public static T IgnoreException<T> (Func<T> func, bool print = true)
+		{
+			try {
+				return func ();
+			} catch (Exception e) {
+				//ignore
+				if (print)
+					Debug.WriteLine (e);
+			}
+			return default(T);
+		}
+
+		public static async Task IgnoreExceptionAsync (Func<Task> func, bool print = true)
+		{
+			try {
+				await func ();
+			} catch (Exception e) {
+				//ignore
+				if (print)
+					Debug.WriteLine (e);
+			}
+		}
+
+		public static async Task<T> IgnoreExceptionAsync<T> (Func<Task<T>> func, bool print = true)
+		{
+			try {
+				return await func ();
+			} catch (Exception e) {
+				//ignore
+				if (print)
+					Debug.WriteLine (e);
+			}
+			return default(T);
+		}
+	}
+}
+
---
+++ BlueMarin.PCL/Source/Helpers/DisposableBoolean.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BlueMarin
+{
+	public class DisposableBoolean : IDisposable
+	{
+		readonly Action<bool> action;
+
+		public DisposableBoolean (Action<bool> action)
+		{
+			this.action = action;
+			action (true);
+		}
+
+		#region IDisposable implementation
+
+		public void Dispose ()
+		{
+			action (false);
+		}
+
+		#endregion
+	}
+}
+
