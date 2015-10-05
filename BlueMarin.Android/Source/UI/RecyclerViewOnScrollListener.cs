ec0c67078dd81fdd8cb697eccda6b57c12a4201a
Benoit Jadinon <raz-l@raz-l.com>
2015-10-06 01:53:40 +0200

    added RecyclerViewOnScrollListener infinite scroll

---
+++ BlueMarin.Android/Source/UI/RecyclerViewOnScrollListener.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Android.Support.V7.Widget;
+
+namespace BlueMarin.Android
+{
+	//https://gist.github.com/martijn00/a45a238c5452a273e602
+	public class RecyclerViewOnScrollListener : RecyclerView.OnScrollListener
+	{
+		public delegate void LoadMoreEventHandler(RecyclerViewOnScrollListener sender, int total);
+		public event LoadMoreEventHandler LoadMoreEvent;
+
+		private readonly LinearLayoutManager LayoutManager;
+
+		public RecyclerViewOnScrollListener (LinearLayoutManager layoutManager)
+		{
+			LayoutManager = layoutManager;
+		}
+
+		public override void OnScrolled (RecyclerView recyclerView, int dx, int dy)
+		{
+			base.OnScrolled (recyclerView, dx, dy);
+
+			var visibleItemCount = recyclerView.ChildCount;
+			var totalItemCount = recyclerView.GetAdapter().ItemCount;
+			var pastVisiblesItems = LayoutManager.FindFirstVisibleItemPosition();
+
+			if ((visibleItemCount + pastVisiblesItems) >= totalItemCount) {
+				LoadMoreEvent (this, totalItemCount);
+			}
+		}
+	}
+}
+
