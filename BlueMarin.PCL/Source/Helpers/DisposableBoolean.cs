f633cc203d3d34ca18afb92d636fcec2511daf44
Benoit Jadinon <raz-l@raz-l.com>
2015-10-06 09:48:14 +0200

    disposableBoolean now works on getters and setters

--- BlueMarin.PCL/Source/Helpers/DisposableBoolean.cs
+++ BlueMarin.PCL/Source/Helpers/DisposableBoolean.cs
@@ -4,19 +4,31 @@ namespace BlueMarin
 {
 	public class DisposableBoolean : IDisposable
 	{
-		readonly Action<bool> action;
+		readonly Action<bool> getter;
+		readonly Func<bool, bool> getterSetter;
 
-		public DisposableBoolean (Action<bool> action)
+		readonly bool initialValue = true;
+
+		public DisposableBoolean (Action<bool> getter, bool initialValue = true)
 		{
-			this.action = action;
-			action (true);
+			this.initialValue = initialValue;
+			this.getter = getter;
+			getter (initialValue);
+		}
+
+		public DisposableBoolean (Func<bool, bool> getterSetter, bool initialValue = true)
+		{
+			this.initialValue = initialValue;
+			this.getterSetter = getterSetter;
+			getterSetter (initialValue);
 		}
 
 		#region IDisposable implementation
 
 		public void Dispose ()
 		{
-			action (false);
+			getterSetter?.Invoke (!initialValue);
+			getter?.Invoke (!initialValue);
 		}
 
 		#endregion
