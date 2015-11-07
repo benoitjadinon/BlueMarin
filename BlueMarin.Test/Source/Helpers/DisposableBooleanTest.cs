e8c26369d0edb01daa77814cfc01f44c89bcf41e
Benoit Jadinon <raz-l@raz-l.com>
2015-11-07 21:28:56 +0100

    added DisposableBoolean test

---
+++ BlueMarin.Test/Source/Helpers/DisposableBooleanTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace BlueMarin.Test
+{
+	[TestFixture ()]
+	public class DisposableBooleanTest
+	{
+		bool TestGetSet { get; set; }
+		bool TestSet { 
+			set{
+				Console.WriteLine (value);
+			}
+		}
+		bool TestGet { get; }
+
+		[Test]
+		public void TestGetterSetter ()
+		{
+			using (DisposableBoolean db = new DisposableBoolean (b => TestGetSet = b)) {
+				Assert.IsTrue (TestGetSet);
+			}
+			Assert.IsFalse (TestGetSet);
+
+			//Assert.IsNull (db);
+		}
+
+
+		[Test]
+		public void TestSetter ()
+		{
+			using (DisposableBoolean db = new DisposableBoolean (b => TestSet = b)) {
+			}
+
+			//Assert.IsNull (db);
+		}
+	}
+}
+
