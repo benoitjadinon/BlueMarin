5bda4a09b6a425b5ed3cc698556f4550ff3e201b
Benoit Jadinon <j@dinonbeno.it>
2016-01-13 23:08:11 +0100

    added Rx

---
+++ BlueMarin.Rx/RxDebugObserver.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reactive.Linq;
+
+namespace BlueMarin.Rx
+{
+	public class RxDebugObserver<T> : IObserver<T>
+	{
+		string id;
+
+		public RxDebugObserver (string id = "Rx Sequence")
+		{
+			this.id = id;			
+		}
+
+		#region IObserver implementation
+
+		public void OnNext (T value)
+		{
+			Debug.WriteLine ("{0} received ", id, value);
+		}
+
+		public void OnCompleted ()
+		{
+			Debug.WriteLine ("{0}", id);
+		}
+
+		public void OnError (Exception error)
+		{
+			Debug.WriteLine ("{0} faulted with ", id, error);
+		}
+
+		#endregion
+	}
+}
+
