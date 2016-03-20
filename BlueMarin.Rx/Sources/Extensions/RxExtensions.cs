58c17e57e6e0fe548309f06a7b54a6dd9428a529
Benoit Jadinon <j@dinonbeno.it>
2016-03-20 11:49:41 +0100

    added Where(bool)

---
+++ BlueMarin.Rx/Sources/Extensions/RxExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace BlueMarin.Rx
+{
+	public static class RxExtensions
+	{
+		public static IObservable<bool> Where (this IObservable<bool> @this, bool value = true)
+		{
+			return @this.Where(v => v == value);
+		}
+	}
+}
+
